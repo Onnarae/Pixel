e619c3e3ed98309a5e4a0dc796857329a2bdf0a1
Onnarae <66655413+Onnarae@users.noreply.github.com>
2021-05-31 18:54:23 +0900

    dto test

---
+++ src/main/java/com/fan/pixel/testproject/Application.java
@@ -0,0 +1,10 @@
+package com.fan.pixel.testproject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
